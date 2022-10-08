5da45c89ac9910d34453395edb46023e1c1d1b7b
savendanogmt <114439803+savendanogmt@users.noreply.github.com>
2022-10-08 16:45:03 -0500

    Add files via upload

---
+++ motorbike/src/main/java/com/reto/motorbike/model/Motorbike.java
@@ -0,0 +1,96 @@
+package com.reto.motorbike.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "motorbike")
+public class Motorbike {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private Integer id;
+    private String name;
+    private String brand;    
+    @Column(name ="years")
+    private Integer year;
+    private String description;
+    
+    @ManyToOne
+    @JoinColumn(name = "categoryId")
+    @JsonIgnoreProperties("motorbikes")
+    private Category category;
+
+
+    @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "motorbike")
+    @JsonIgnoreProperties({"motorbike","client"})
+    private List<Message> messages;
+
+    @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "motorbike")
+    @JsonIgnoreProperties({"motorbike","messages"})
+    public List<Reservation> reservations;
+    
+    public Category getCategory() {
+        return category;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+    public List<Message> getMessages() {
+        return messages;
+    }
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+ 
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getYear() {
+        return year;
+    }
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+}
